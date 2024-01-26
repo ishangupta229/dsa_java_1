954ede92afd2bdd383132bc2bffd3aee512fbd0b
ishangupta229 <121247230+ishangupta229@users.noreply.github.com>
2024-01-26 23:38:06 +0530

    Update stocks.java

--- stocks.java
+++ stocks.java
@@ -6,13 +6,20 @@ public class stocks {
             min_price[i] = min_price[i-1]>arry[i] ? arry[i]:min_price[i-1] ;
         }
         int day_profit[] = new int[arry.length];
+        int total_profit=0;
+        int max_profit=0;
         for (int i = 0; i < arry.length; i++) {
             day_profit[i] = arry[i]- min_price[i];
+            total_profit += day_profit[i];
+            max_profit = day_profit[i]>max_profit? day_profit[i]:max_profit;
         }
         for (int i = 0; i < day_profit.length; i++) {
             System.out.print(day_profit[i]);
 
-        }System.out.println();
+        }
+        System.out.println();
+        System.out.println("Total profit is : " + total_profit);
+        System.out.println("Max profit is : " + max_profit);
     }
 
     public static void main(String[] args) {
