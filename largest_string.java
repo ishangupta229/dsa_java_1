caf53ab71a21a84b573879eaae418e64958ed08c
ishangupta229 <121247230+ishangupta229@users.noreply.github.com>
2024-02-02 13:32:29 +0530

    Create largest_string.java

---
+++ largest_string.java
@@ -0,0 +1,14 @@
+// Lexicographic
+
+public class largest_string {
+    public static void main(String[] args) {
+        String fruits[] = { "Apple" , "Mango", "Banana","Pineapple", "Ananas"};
+        String largest = fruits[0];
+        for (int i = 1; i < fruits.length; i++) {
+            if (largest.compareToIgnoreCase(fruits[i])<0) {
+                largest = fruits[i];
+            }
+        }
+        System.out.println(largest);
+    }   
+}
